4ff64e7e9d10ba68b79f05f5c7667065e4e043fa
Alexandr <schurik1983@bk.ru>
2023-10-15 19:08:07 +0300

    finish-2 commit

--- task3/Program.cs
+++ task3/Program.cs
@@ -17,17 +17,13 @@ while (true)
     int enterDigit = getDigit("Введите порядковый номер дня недели, цифра должна быть не от 1 до 7");
     if (0 <enterDigit & enterDigit < 8)
         {
-            string[] daysWeeks = {
-                "Понедельник",
-                "Вторник",
-                "Среда",
-                "Четверг",
-                "Пятница",
-                "Суббота",
-                "Воскресение"
-            };
-            Console.WriteLine($"Ваш день {daysWeeks[enterDigit - 1]}");
-            break;
+        if (enterDigit == 6 & enterDigit == 7)
+            {
+                Console.WriteLine("Да, сегодня выходной");
+            }
+        else{
+            Console.WriteLine("Нет, сегодня рабочий день");
+        }
         }
         else 
         {
